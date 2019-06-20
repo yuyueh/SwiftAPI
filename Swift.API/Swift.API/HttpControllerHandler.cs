4169632e1b4e7ed3c0a0b15592835b5ee88fd1a1
ed.chang <ed.chang@xuenn.com>
2019-06-21 00:31:36 +0800

    version 1

---
+++ Swift.API/Swift.API/HttpControllerHandler.cs
@@ -0,0 +1,17 @@
+﻿using System.Web;
+
+namespace Swift.API
+{
+    public class HttpControllerHandler  : IHttpHandler
+    {
+        public bool IsReusable
+        { 
+            get { return false; }
+        }
+
+        public void ProcessRequest(HttpContext context)
+        {
+            context.Response.Write("Hello World!");
+        }
+    }
+}
---
+++ Swift.API/Swift.API/UrlRoutingModule.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Web;
+
+namespace Swift.API
+{
+    public class UrlRoutingModule : IHttpModule
+    {
+        public void Dispose()
+        {
+            //throw new NotImplementedException();
+        }
+
+        public void Init(HttpApplication app)
+        {
+            app.PostResolveRequestCache += app_PostResolveRequestCache;
+        }
+
+        void app_PostResolveRequestCache(object sender, EventArgs e)
+        {
+            var app = (HttpApplication)sender;
+
+            app.Context.RemapHandler(new HttpControllerHandler());
+        }
+    }
+}
